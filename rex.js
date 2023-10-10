d50d464acc1874a90c077b0b8bae9ed3444417a6
Aindrik Sarkar <119431351+iamaindrik@users.noreply.github.com>
2023-10-10 20:38:03 +0530

    Add files via upload

---
+++ rex.js
@@ -0,0 +1,60 @@
+function tikka() {
+  window.open("https://www.youtube.com/watch?v=kZxmErE7V2E", "_blank");
+}
+
+function butter() {
+  window.open("https://www.youtube.com/watch?v=a30BLUQiFoc", "_blank");
+}
+
+function chili() {
+  window.open("https://www.youtube.com/watch?v=P7ZGxWKJkX8", "_blank");
+}
+
+function redsauce() {
+  window.open("https://www.youtube.com/watch?v=K1P_KI1YD4c", "_blank");
+}
+
+function whitesauce() {
+  window.open("https://www.youtube.com/watch?v=l_HOohJZL0U", "_blank");
+}
+
+function masala() {
+  window.open("https://www.youtube.com/watch?v=UjDfZDsKz2M", "_blank");
+}
+
+function tawap() {
+  window.open("https://youtu.be/zAICu_dIlUs?feature=shared", "_blank");
+}
+
+function pulao() {
+  window.open("https://youtu.be/c0BeFR-wMi4?feature=shared", "_blank");
+}
+
+function anda() {
+  window.open("https://youtu.be/vPkm5GnTE_8?feature=shared", "_blank");
+}
+
+function next() {
+    var url = 'rex2.html';
+    window.open(url);
+}
+
+function next2() {
+  var url = 'rex3.html';
+  window.open(url);
+}
+
+function next3() {
+  var url = 'new.html';
+  window.open(url);
+}
+
+function prev() {
+  var url = 'rexhome.html';
+  window.open(url);
+}
+
+function prev2() {
+  var url = 'rex2.html';
+  window.open(url);
+}
